bd4906941775ae5e49812cd372635b14311f4e0b
bullert <b.bullert5@gmail.com>
2022-04-05 00:18:43 +0200

    Create & apply a db schema for the product model

--- ProductAPI/Data/ApiDbContext.cs
+++ ProductAPI/Data/ApiDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using ProductAPI.Data.Configurations;
 using ProductAPI.Models;
 using System;
 using System.Collections.Generic;
@@ -16,5 +17,11 @@ namespace ProductAPI.Data
         }
 
         public DbSet<Product>? Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.ApplyConfiguration(new ProductConfiguration());
+        }
     }
 }
---
+++ ProductAPI/Data/Configurations/ProductConfiguration.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ProductAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductAPI.Data.Configurations
+{
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Name)
+                .IsRequired()
+                .HasColumnType("varchar")
+                .HasMaxLength(100);
+
+            builder.Property(x => x.Description)
+                .HasColumnType("varchar")
+                .HasMaxLength(200);
+
+            builder.Property(x => x.Price)
+                .IsRequired()
+                .HasPrecision(12, 10);
+        }
+    }
+}

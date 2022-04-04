01c5a84ca235ecd522fc46899f37dec7f10cce07
bullert <b.bullert5@gmail.com>
2022-04-04 21:59:30 +0200

    Add a database context

---
+++ ProductAPI/Data/ApiDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductAPI.Data
+{
+    public class ApiDbContext : DbContext
+    {
+        public ApiDbContext(DbContextOptions<ApiDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Product>? Products { get; set; }
+    }
+}
--- ProductAPI/Program.cs
+++ ProductAPI/Program.cs
@@ -1,6 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using ProductAPI.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<ApiDbContext>(opt => opt.UseInMemoryDatabase("Products"));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle

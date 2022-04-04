31f4d17333a852e274be48ffd33f438fcfb1b31d
bullert <b.bullert5@gmail.com>
2022-04-04 22:04:56 +0200

    Add a repository base class

---
+++ ProductAPI/Data/Repositories/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq.Expressions;
+
+namespace ProductAPI.Data.Repositories
+{
+    public interface IRepository<T> where T : class
+    {
+        Task AddAsync(T entity);
+        Task AddRangeAsync(IEnumerable<T> entities);
+        Task<int> CountAsync();
+        Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate);
+        Task<T?> FindSingleOrDefaultAsync(Expression<Func<T, bool>> predicate);
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T?> GetByIdAsync(Guid id);
+        void Remove(T entity);
+        void RemoveRange(IEnumerable<T> entities);
+    }
+}
---
+++ ProductAPI/Data/Repositories/Repository.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductAPI.Data.Repositories
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        protected readonly DbContext context;
+
+        public Repository(DbContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<T?> GetByIdAsync(Guid id)
+        {
+            return await context.Set<T>().FindAsync(id);
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await context.Set<T>().ToListAsync();
+        }
+
+        public async Task AddAsync(T entity)
+        {
+            await context.Set<T>().AddAsync(entity);
+        }
+
+        public async Task AddRangeAsync(IEnumerable<T> entities)
+        {
+            await context.Set<T>().AddRangeAsync(entities);
+        }
+
+        public void Remove(T entity)
+        {
+            context.Set<T>().Remove(entity);
+        }
+
+        public void RemoveRange(IEnumerable<T> entities)
+        {
+            context.Set<T>().RemoveRange(entities);
+        }
+
+        public async Task<T?> FindSingleOrDefaultAsync(Expression<Func<T, bool>> predicate)
+        {
+            return await context.Set<T>().SingleOrDefaultAsync(predicate);
+        }
+
+        public async Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate)
+        {
+            return await context.Set<T>().Where(predicate).ToListAsync();
+        }
+
+        public async Task<int> CountAsync()
+        {
+            return await context.Set<T>().CountAsync();
+        }
+    }
+}

4a6e78d5ea719eaec67ac350ab0be75e520ccb71
bullert <b.bullert5@gmail.com>
2022-04-04 22:06:01 +0200

    Add a product repository

---
+++ ProductAPI/Data/Repositories/ProductRepository.cs
@@ -0,0 +1,19 @@
+﻿using ProductAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductAPI.Data.Repositories
+{
+    public class ProductRepository : Repository<Product>, IProductRepository
+    {
+        public ProductRepository(ApiDbContext appDbContext) : base(appDbContext)
+        {
+
+        }
+
+        private ApiDbContext? Context => context as ApiDbContext;
+    }
+}

ca818f48a5e932918469a2e3a3ed14a647ab56fc
bullert <b.bullert5@gmail.com>
2022-04-04 21:50:59 +0200

    Add a product model

---
+++ ProductAPI/Models/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductAPI.Models
+{
+    public class Product
+    {
+        public Guid Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public int Number { get; set; }
+
+        public int Quantity { get; set; }
+
+        public string? Description { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}

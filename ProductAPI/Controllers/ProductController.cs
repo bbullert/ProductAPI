aa900503cf59bf741af13dea6b5c07bd894a7260
bullert <b.bullert5@gmail.com>
2022-04-05 00:31:38 +0200

    Create a product controller

---
+++ ProductAPI/Controllers/ProductController.cs
@@ -0,0 +1,111 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProductAPI.Helpers.Exceptions;
+using ProductAPI.Models;
+using ProductAPI.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductAPI.Controllers
+{
+    [ApiController]
+    [Route("/api/[controller]")]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductService productService;
+
+        public ProductController(IProductService productService)
+        {
+            this.productService = productService;
+
+            Task.Run(async () => {
+                await productService.SetExampleDataAsync();
+            }).Wait();
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        public async Task<ActionResult<IEnumerable<Product>>> Get()
+        {
+            var products = await productService.GetAllProducts();
+
+            if (!products.Any())
+            {
+                return NoContent();
+            }
+
+            return Ok(products);
+        }
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<Product>> Get(Guid id)
+        {
+            var product = await productService.GetProductById(id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(product);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<Guid>> GuidPost(Product product)
+        {
+            Guid id;
+
+            try
+            {
+                id = await productService.CreateProduct(product);
+            }
+            catch (ArgumentException)
+            {
+                return StatusCode(500);
+            }
+
+            return StatusCode(201, id);
+        }
+
+        [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult> Put(Guid id, Product product)
+        {
+            try
+            {
+                await productService.UpdateProduct(id, product);
+            }
+            catch (ProductNotExist)
+            {
+                return NotFound();
+            }
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult> Delete(Guid id)
+        {
+            try
+            {
+                await productService.RemoveProduct(id);
+            }
+            catch (ProductNotExist)
+            {
+                return NotFound();
+            }
+
+            return NoContent();
+        }
+    }
+}

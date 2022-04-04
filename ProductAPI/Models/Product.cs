e15b6a347a48590a839176292483966310b4adea
bullert <b.bullert5@gmail.com>
2022-04-05 00:11:15 +0200

    Add a basic validation for the product model

--- ProductAPI/Models/Product.cs
+++ ProductAPI/Models/Product.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,14 +13,19 @@ namespace ProductAPI.Models
     {
         public Guid Id { get; set; }
 
+        [Required]
+        [MaxLength(100)]
         public string? Name { get; set; }
 
         public int Number { get; set; }
 
         public int Quantity { get; set; }
 
+        [MaxLength(200)]
         public string? Description { get; set; }
 
-        public decimal Price { get; set; }
+        [Required]
+        [Range(0, double.MaxValue, ErrorMessage = "The field Price must be a positive number.")]
+        public decimal? Price { get; set; }
     }
 }

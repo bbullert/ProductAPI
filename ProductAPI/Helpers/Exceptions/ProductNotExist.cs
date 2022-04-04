5e70be8cb0f3a45720302833f9d23cd7aa926511
bullert <b.bullert5@gmail.com>
2022-04-05 00:29:08 +0200

    Create a ProductNotExist exception

---
+++ ProductAPI/Helpers/Exceptions/ProductNotExist.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductAPI.Helpers.Exceptions
+{
+    public class ProductNotExist : Exception
+    {
+        public ProductNotExist()
+        {
+
+        }
+
+        public ProductNotExist(string message) : base(message)
+        {
+
+        }
+
+        public ProductNotExist(Guid id) : base($"The product with an id: \"{id}\" doesn't exist.")
+        {
+
+        }
+    }
+}
